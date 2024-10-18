433778e6605cddcd9740e605e1e438896259f17f
睡姿决定发型 <hqxws@shuizijuedingfaxingdeMacBook-Pro.local>
2024-10-18 11:25:28 -0700

    update

--- Tests/Lab-3Tests/CounterTests.swift
+++ Tests/Lab-3Tests/CounterTests.swift
@@ -2,20 +2,15 @@ import XCTest
 
 @testable import Lab_3
 
-final class CounterTests: XCTestCase {
 func testIncrement() {
-let counter = Counter()
-counter.Increment()
+var counter = Counter()
+counter.increment()
 print("Counter value after increment: \(counter.value)") // Print the counter value
 XCTAssertEqual(counter.value, 1, "Counter increment failed")
-	}
 }
-
-final class CounterTests: XCTestCase {
 func testDecrement() {
 var counter = Counter()
-counter.Decrement()
+counter.decrement()
 print("Counter value after decrement: \(counter.value)") // Print the counter value
 XCTAssertEqual(counter.value, -1, "Counter decrement failed")
-}
 }
